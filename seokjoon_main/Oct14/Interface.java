3c89bc542d3adc610c682ad8a3cbdfd926cc8569
joon <sjchung@Joon-2.local>
2022-10-15 23:40:22 +0900

    Oct15Update

--- seokjoon_main/Oct14/Interface.java
+++ seokjoon_main/Oct14/Interface.java
@@ -2,6 +2,11 @@ interface Predator{
     String GetFood();
 }
 
+interface Barkable{
+    String GetBark();
+}
+
+
 class Animals {
     String name;
 
@@ -11,20 +16,28 @@ class Animals {
     }
 }
 
-class Tiger extends Animals implements Predator{
+class Tiger extends Animals implements Predator, Barkable {
+
+    Tiger(String name){
+        this.SetName(name);
+    }
+
     public String GetFood() {
         return "호랑이밥";
 
     }
+
+    public String GetBark() {
+        return "우흥~~";
+    }
 }
 
-class Lion extends Animals implements Predator{
-    public String GetFood(){
+class Lion extends Animals implements Predator {
+
+    public String GetFood() {
         return "사자밥";
     }
-
 }
-
 class Leopard extends Animals implements Predator {
 
     public String GetFood() {
@@ -41,17 +54,35 @@ class Feeder{
     }
 
 }
+
+class BarkingMaker {
+    void Barking(Barkable animal){
+        System.out.printf(animal.GetBark());
+
+    }
+}
 public class Interface {
     public static void  main (String[] args){
-        Tiger tiger1 = new Tiger();
+        Tiger tiger1 = new Tiger("Fucker");
         Lion lion1 = new Lion();
+        lion1.SetName("Eon");
         Leopard leopard1 = new Leopard();
+
+
         Feeder feeder1 = new Feeder();
 
-        feeder1.Feeding(tiger1);
 
+        System.out.println("name of tiger1 is " + tiger1.name ); // dot operator 를 이용해 객체 tiger1의 name 에 접근하였다. name은 animal class의 class variable 이다.
+
+
+        feeder1.Feeding(tiger1);
         feeder1.Feeding(lion1);
         feeder1.Feeding(leopard1);
+
+
+        BarkingMaker maker1 = new BarkingMaker();
+        maker1.Barking(tiger1);
+
 
     }
 }
---
+++ seokjoon_main/Oct14/Polymorphism.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+public class Polymorphism {
+    public static void  main(String[] args){
+
+
+    }
+}
